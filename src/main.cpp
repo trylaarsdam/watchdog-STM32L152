50fb28d61d44798d641e440a641810a84464e279
Todd Rylaarsdam <todd@toddr.org>
2021-12-01 09:40:12 -0600

    fixed dep order

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
-#include "watchdog.hpp"
 #include <Arduino.h>
+#include "watchdog.hpp"
 #include "stm32l1xx_hal_rcc.h"
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(D12, OUTPUT);
+  pinMode(D8, OUTPUT);
   if(__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
-    digitalWrite(D12, HIGH);
+    digitalWrite(D8, LOW);
   }
   else {
-    digitalWrite(D12, LOW);
+    digitalWrite(D8, LOW);
   }
+  __HAL_RCC_CLEAR_RESET_FLAGS();
   pinMode(USER_BTN, INPUT);
   watchdogInit(5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   while(!digitalRead(USER_BTN)) {
     watchdogPet();
   }
