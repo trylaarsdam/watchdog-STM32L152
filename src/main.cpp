0d814c2e1a568f7add01641a2b7502ea93cc892a
Todd Rylaarsdam <todd@toddr.org>
2021-12-01 09:18:18 -0600

    pet watchdog until button press

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+#include "watchdog.hpp"
 #include <Arduino.h>
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(D12, OUTPUT);
+  pinMode(USER_BTN, INPUT);
+  watchdogInit(5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(!digitalRead(USER_BTN)) {
+    watchdogPet();
+  }
+  
 }
