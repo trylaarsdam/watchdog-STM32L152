ae2055aacde567d0cf005559de3ecbdbc615d448
Todd Rylaarsdam <todd@toddr.org>
2021-12-01 10:05:25 -0600

    fixed iwdg handle being declared twice

--- src/watchdog.hpp
+++ src/watchdog.hpp
@@ -5,11 +5,11 @@
 #include "constants.hpp"
 
 #define kWatchdogPulsesPerSecond 155 // assuming LSE at 32.768kHz, prescaler = 256, 155 * seconds gives an accurate reload value
-IWDG_HandleTypeDef IwdgHandle;
 
 class WatchDog {
     private:
         bool watchdogEnabled = false;
+        IWDG_HandleTypeDef IwdgHandle;
     public:
         void init(uint32_t secondsToTimeout);
         void pet();
