4ace9266560c8c92897d14837fd66712c07b6e4b
Todd Rylaarsdam <todd@toddr.org>
2021-12-01 09:20:32 -0600

    test for watchdog reset flag on startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "watchdog.hpp"
 #include <Arduino.h>
+#include "stm32l1xx_hal_rcc.h"
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(D12, OUTPUT);
+  if(__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
+    digitalWrite(D12, HIGH);
+  }
+  else {
+    digitalWrite(D12, LOW);
+  }
   pinMode(USER_BTN, INPUT);
   watchdogInit(5);
 }
@@ -13,5 +20,8 @@ void loop() {
   while(!digitalRead(USER_BTN)) {
     watchdogPet();
   }
-  
+
+  while(1) {
+
+  }
 }
