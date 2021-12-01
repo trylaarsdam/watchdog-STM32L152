81f1af0a5f256859fdd0b5a79756ea409db32e57
Todd Rylaarsdam <todd@toddr.org>
2021-12-01 09:59:38 -0600

    comments

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,2 +1,1 @@
 #pragma once
-#define kWatchdogPulsesPerSecond 155
--- src/watchdog.hpp
+++ src/watchdog.hpp
@@ -4,16 +4,10 @@
 #include <stm32l1xx_hal_wwdg.h>
 #include "constants.hpp"
 
+#define kWatchdogPulsesPerSecond 155 // assuming LSE at 32.768kHz, prescaler = 256, 155 * seconds gives an accurate reload value
 IWDG_HandleTypeDef IwdgHandle;
 
 void watchdogInit(uint32_t secondsToTimeout) {
-  //Assuming LSI runs on 32kHz and we want the 
-	//max watchdog value, we use the highest prescaler of 
-	//256 to get 32000 Hz / 256 = 125 Hz (watchdog clock pulses per second)
-	//we use the maximum reload value of 0x0FFF = 4095. 
-	//meaning that the watchdog has a max timeout of 4095 / 125 = 32.76 seconds. 
-	//If HAL_IWDG_Refresh() is not called before this timeout expires, the micro-
-	//controller resets.
   IwdgHandle.Instance = IWDG;
 	IwdgHandle.Init.Prescaler = IWDG_PRESCALER_256;
 	IwdgHandle.Init.Reload    = secondsToTimeout * kWatchdogPulsesPerSecond;
@@ -25,5 +19,5 @@ void watchdogInit(uint32_t secondsToTimeout) {
 }
 
 void watchdogPet() {
-  HAL_IWDG_Refresh(&IwdgHandle);
+  HAL_IWDG_Refresh(&IwdgHandle); // resets the IWDG counter back to 0
 }
